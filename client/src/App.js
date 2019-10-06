411ed850d97e2b049f59cfb0bf2594741b9dfaf4
Pranav Bedre <bedrepranav@gmail.com>
2019-10-06 23:16:45 +0530

    Initial dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,75 +1,81 @@
-import React, { Component } from 'react';
-import { Route, Switch,Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 // import { withRouter } from 'react-router-dom';
-import Signup from './containers/signup/signup';
-import Login from './containers/login/login';
-import Landing from './containers/landing/landing';
-import {connect} from 'react-redux';
-import * as actions from './store/actions/index';
-
+import Signup from "./containers/signup/signup";
+import Login from "./containers/login/login";
+import Landing from "./containers/landing/landing";
+import Dashboard from "./containers/dashboard/dashboard";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = {
-
-    }
+    super();
+    this.state = {};
   }
 
   componentDidMount() {
     this.props.onTryAutoSignIn();
   }
 
-  componentDidUpdate() {
-
-  }
-
+  componentDidUpdate() {}
 
   render() {
     let routes;
-    if(this.props.loggedIn){
+    if (this.props.loggedIn) {
       routes = (
-           <Switch>
-            <Route path="/" exact render={() => <Landing loggedIn={this.props.loggedIn}/>}/>
-            <Redirect to="/" />
-           </Switch>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => <Landing loggedIn={this.props.loggedIn} />}
+          />
+          <Route path="/dashboard" render={() => <Dashboard />} />
+          <Redirect to="/" />
+        </Switch>
       );
-    }else{
+    } else {
       routes = (
-         <Switch>
-            <Route path="/" exact render={() => <Landing loggedIn={this.props.loggedIn}/>}/>
-            <Route path="/signup" render={() => <Signup/>}/>
-            <Route path="/login"  render={() => <Login/>} />
-            <Redirect to="/" />
-         </Switch>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => <Landing loggedIn={this.props.loggedIn} />}
+          />
+          <Route path="/signup" render={() => <Signup />} />
+          <Route path="/dashboard" render={() => <Dashboard />} />
+          <Route path="/login" render={() => <Login />} />
+          <Redirect to="/" />
+        </Switch>
       );
     }
     return (
       <div>
-        <header>
-             {routes}
-        </header>
+        <header>{routes}</header>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch =>{
-  return{
-    onTryAutoSignIn: () => dispatch( actions.authCheckState() )
-  }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignIn: () => dispatch(actions.authCheckState())
+  };
+};
 
-const mapStateToProps = state =>{
-  if(state.user === null){
-    console.log('no user is present in session');
-  }else{
-    return{
-      user:state.auth.user,
-      loggedIn:state.auth.loggedIn,
-      token:state.auth.token
-    }
+const mapStateToProps = state => {
+  if (state.user === null) {
+    console.log("no user is present in session");
+  } else {
+    return {
+      user: state.auth.user,
+      loggedIn: state.auth.loggedIn,
+      token: state.auth.token
+    };
   }
-}
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);

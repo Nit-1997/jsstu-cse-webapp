041d5ab899e9847ff3c5d6054f82bbc7af2720c2
Binu <kbinu42@gmail.com>
2019-11-09 15:59:07 +0530

    spinner added for add, edit and delete of publications

--- client/src/containers/dashboard/publication/cardList.js
+++ client/src/containers/dashboard/publication/cardList.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import Card from "./card"
+import Spinner from '../../spinner/Spinners'
 import "./form.css"
 
 export default class Cardlist extends Component {
 
     render() {
+        if(this.props.loading) return <Spinner/>
         return (
             <section className="cardList">
                 {
                     this.props.publications.map(card => (
-                        <Card key={card._id} card={card} user={this.props.user} removeCard={this.props.removeCard} publishEdit={this.props.publishEdit}></Card>
+                        <Card loading={this.props.loading} key={card._id} card={card} user={this.props.user} removeCard={this.props.removeCard} publishEdit={this.props.publishEdit}></Card>
                     )
                     )
                 }
